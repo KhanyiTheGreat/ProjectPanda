cdc43737af388241e47649c0bf1973c7c4f82127
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-11-04 17:29:32 +0200

    removed the virtual method in the appointment class cause it does not inheret from any other class

--- ProjectPanda/ProjectPanda/Models/Appointment.cs
+++ ProjectPanda/ProjectPanda/Models/Appointment.cs
@@ -8,13 +8,13 @@ namespace ProjectPanda.Models
 
     {
 
-        public virtual void BookAppointment()
+        public void BookAppointment()
         {
 
             //This method will be for making an appointment  
         }
 
-        public void CancelAppointment()
+        public  void CancelAppointment()
         {
             //This method is for cancelling the already booked appointment
             //this method will have to be overid by the BookAppiontment method
