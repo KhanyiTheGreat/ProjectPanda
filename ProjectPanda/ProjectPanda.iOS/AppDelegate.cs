d8cc79bd03729aa62b7f5f02bfe4ed71df812cb8
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-11-09 20:12:10 +0200

    removed all the code that is not needed

--- ProjectPanda/ProjectPanda.iOS/AppDelegate.cs
+++ ProjectPanda/ProjectPanda.iOS/AppDelegate.cs
@@ -27,8 +27,10 @@ namespace ProjectPanda.iOS
             Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
             // This MobileServiceClient has been configured to communicate with the Azure Mobile App and
             // Azure Gateway using the application url. You're all set to start working with your Mobile App!
-            Microsoft.WindowsAzure.MobileServices.MobileServiceClient ProjectPandaMobileAppService20171016015234Client = new Microsoft.WindowsAzure.MobileServices.MobileServiceClient(
-            "https://projectpandamobileappservice20171016015234.azurewebsites.net");
+            Microsoft.WindowsAzure.MobileServices.MobileServiceClient ProjectPandaClient = new Microsoft.WindowsAzure.MobileServices.MobileServiceClient(
+            "https://projectpanda.azurewebsites.net");
+
+        
             global::Xamarin.Forms.Forms.Init ();
 
             //GPs code for Ios
