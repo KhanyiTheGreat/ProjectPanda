8c3c90d1cf80d3826987edff3d859cb964bd480e
Bonga <Bonga@DESKTOP-8R6GJO0>
2017-11-10 00:08:26 +0200

    first commit :)

--- ProjectPanda/ProjectPanda/Models/DocAvaliable.cs
+++ ProjectPanda/ProjectPanda/Models/DocAvaliable.cs
@@ -62,6 +62,11 @@ namespace ProjectPanda.Models
 
 
         };
+
+        internal void Add(DocAvaliable doctorOnCall)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
 
     }
