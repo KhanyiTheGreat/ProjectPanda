30994a156711794015747580d106477e0b902aef
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-11-12 00:01:19 +0200

    still working on some newly created file

---
+++ ProjectPanda/ProjectPanda/Models/RegisteredPatients.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPanda.Models
+{
+    class RegisteredPatients
+    {
+        [Newtonsoft.Json.JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Version]
+        public string AzureVersion { get; set; }
+
+
+
+
+
+
+    }
+}
