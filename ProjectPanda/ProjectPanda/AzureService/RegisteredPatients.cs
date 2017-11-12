d5af9a681f1752fdb2a3096ca5fdf322ed3d7b28
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-11-12 03:11:17 +0200

    configuring the tables and the sycnrinzatin of those tables

---
+++ ProjectPanda/ProjectPanda/AzureService/RegisteredPatients.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectPanda.Models;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using ProjectPanda.Models;
+
+namespace ProjectPanda.AzureService
+{
+
+    
+
+    public class RegisteredPatients
+    {
+
+        //This is going to help syncronize the back end with the front when it comes to updating the users settings, especially the personal information section
+
+
+        MobileServiceClient client = null;      //will help the syncronization between what is on our servers and what is being displayed on the customers screen
+
+        IMobileServiceTable<RegisteredPatients> patientInfo;
+
+                        public async Task Initalize() {
+
+                                if (client?.SyncContext?.IsInitialized ?? false) { return; }
+
+                                var ProjectPandaUrl = "http://projectpanda.azurewebsites.net";
+
+                                client = new MobileServiceClient(ProjectPandaUrl);
+
+                            var localDeviceDB = "UserDevice.Db";
+
+                            var store = new MobileServiceSQLiteStore(localDeviceDB);
+
+                             store.DefineTable<RegisteredPatients> (); //i'll uncomment this later once I understand what james is saying
+
+                            
+
+                        }
+
+
+
+
+             
+
+
+    }
+}
--- ProjectPanda/ProjectPanda/Models/RegisteredPatients.cs
+++ ProjectPanda/ProjectPanda/Models/RegisteredPatients.cs
@@ -1,13 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Newtonsoft.Json;
 namespace ProjectPanda.Models
 {
     class RegisteredPatients
     {
-        [Newtonsoft.Json.JsonProperty("Id")]
+        [JsonProperty("Id")]
         public string Id { get; set; }
+
+
+        [JsonProperty("MedicalAid")]
+        public string MedicalAid { get; set; }
+
+
+        [JsonProperty("BloodType")]
+        public string BloddType { get; set; }
+
+        [JsonProperty("FrequentDoctor")]
+        public string ChronicDisease { get; set; }
+
+        [JsonProperty("DosagePerDay")]                  //The dosage per day will just be concatinated with the medicine taken on a line by line basis.
+        public string DosagePerDay { get; set; }
+
+
+        [JsonProperty("MedicineTaken")]
+        public string MedicineTaken { get; set; }
+
+        [JsonProperty("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+
+
+        [Microsoft.WindowsAzure.MobileServices.CreatedAt]
+        public string AzureCreated { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.UpdatedAt]
+        public string AzureUpdated { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Deleted]
+        public string AzureDeleted { get; set; }
 
         [Microsoft.WindowsAzure.MobileServices.Version]
         public string AzureVersion { get; set; }
