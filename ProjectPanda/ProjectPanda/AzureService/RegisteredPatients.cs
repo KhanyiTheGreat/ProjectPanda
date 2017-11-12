d80ca6e7c5b672cd5898e3e79923bb82c7b62d97
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-11-12 04:33:06 +0200

    I made the code to intialize the backend databse with the local one to store the values on the device and for syncing with the server, still editing those classes.

--- ProjectPanda/ProjectPanda/AzureService/RegisteredPatients.cs
+++ ProjectPanda/ProjectPanda/AzureService/RegisteredPatients.cs
@@ -26,24 +26,97 @@ namespace ProjectPanda.AzureService
 
                                 if (client?.SyncContext?.IsInitialized ?? false) { return; }
 
-                                var ProjectPandaUrl = "http://projectpanda.azurewebsites.net";
+                                var ProjectPandaUrl = "http://projectpanda.azurewebsites.net";  //still need receptors error handles for authentication but will work on those later
 
-                                client = new MobileServiceClient(ProjectPandaUrl);
+                                client = new MobileServiceClient(ProjectPandaUrl);  
 
-                            var localDeviceDB = "UserDevice.Db";
+                            var localDeviceDB = "UserDevice.db";        //self discribed, it sets those values on the device
+
+                          //  MobileServiceClient.EnsureFileExists(localDeviceDB); Makes sure to append where the argument is stored, i'll see if i'll need it in the future to come;  
 
                             var store = new MobileServiceSQLiteStore(localDeviceDB);
 
                              store.DefineTable<RegisteredPatients> (); //i'll uncomment this later once I understand what james is saying
 
-                            
+                             //add more store.defineTable 
 
-                        }
+                          await client.SyncContext.InitializeAsync(store);
+
+                            //egisteredPatients=client.GetSyncTable<>  //for syncrinization 
+
+                          //  client.GetTable(name of the table that needs syncing, i'll represent it as a % sign in the rest of the code);
+                      
+                         }
 
 
+    //The following method is for ensuring that both the online and offline data match
+    public async Task SyncPatientInfo() {
 
 
+           /* 
+                await Initialize();
+
+            try{
+            
+            This will skip becuase it wont be edited and it wont be online
+            if(!CrossConncectivity.Current.IsConnected)
+            return;
+           
+            This will execute if online
+            await client.SyncContext.PushAsync();
+            
+            }
+
+            catch(Exception ex){
+            
+                    debug
+            }
              
+             */
+
+
+
+        }
+
+        
+
+
+        /*
+       
+        FOR NOW i AM GOING TO TYPE THE REST OF WHAT i DONT UNDERSTAND IN THIS COOMENT SECTION CAUSE I CANT THINK RIGHT NOW
+        //Gets the information on the backend side of things
+
+       
+        
+        public async Task<IEnumerable<RegisteredPatients>> GetRegisteredPatients() {
+
+            await Initalize();
+
+            await SyncPatientInfo();
+                
+                var dataFromTable=%;
+        
+        
+                return dataFromTable;
+        
+        }
+
+      
+        //this will simply edit information to be updated to the database if there is any information change 
+       public async Task <RegisteredPatients> EditInformation(){
+       
+        
+        await Intitialize();
+        
+        var patient=new RegisteredPatient{};
+        
+        return null;
+        
+        }        
+         
+        
+         
+         */
 
 
     }
