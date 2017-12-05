234bd0531e5686f4e04c018502cd599dbca2cafa
Bonga <Bonga@DESKTOP-8R6GJO0>
2017-12-05 02:11:18 +0200

    ,,,

--- ProjectPanda/ProjectPanda/Models/MedicalBuildingDataModel.cs
+++ ProjectPanda/ProjectPanda/Models/MedicalBuildingDataModel.cs
@@ -7,7 +7,7 @@ namespace ProjectPanda.Models
     public class MedicalBuildingDataModel
     {
         public string HeadTitle { get; set; } //must change naming
-        public string HeadLines { get; set; }
+        public string HospBio { get; set; }
         public string HeadLinesDesc { get; set; }
         public string ProfileImage { get; set; }
 
--- ProjectPanda/ProjectPanda/ViewModels/MedicalBuildingViewModel.cs
+++ ProjectPanda/ProjectPanda/ViewModels/MedicalBuildingViewModel.cs
@@ -7,7 +7,7 @@ using System.Collections.ObjectModel;
 
 namespace ProjectPanda.ViewModels
 {
-    public class MedicalBuildingViewModel
+    public class MedicalBuildingViewModel : BaseViewModel
     {
         public IList<MedicalBuildingDataModel> CardDataCollection { get; set; }
 
@@ -28,7 +28,7 @@ namespace ProjectPanda.ViewModels
                     {
                      HeadTitle = " Activity rate is 150% - Clermont Clinic",
 
-                     HeadLines="Clermont Clinic. We are a government/public organisation. We are a provincial primary healthcare facility providing HIV and TB-related treatment, care and support services." ,
+                     HospBio="Clermont Clinic. We are a government/public organisation. We are a provincial primary healthcare facility providing HIV and TB-related treatment, care and support services." ,
                      ProfileImage = "Person_7.jpg",
                      HeadLinesDesc = "",
 
@@ -39,7 +39,7 @@ namespace ProjectPanda.ViewModels
                      new MedicalBuildingDataModel
                     {
                      HeadTitle = " Activity rate is 15%  - Groote Schuur Hospital",
-                     HeadLines= " Groote Schuur Hospital is a large, government-funded, teaching hospital situated on the slopes of Devil's Peak in the city of Cape Town, South" ,
+                     HospBio= " Groote Schuur Hospital is a large, government-funded, teaching hospital situated on the slopes of Devil's Peak in the city of Cape Town, South" ,
                      HeadLinesDesc = "The Independent is a British online newspaper.[2]............small sample based on your scenario. Please review my project and let us know",
 
                      ProfileImage = "Person_2.png"
@@ -50,7 +50,7 @@ namespace ProjectPanda.ViewModels
                       new MedicalBuildingDataModel
                     {
                     HeadTitle       = "Activity rate is 15% - Grey's Hospital",
-                    HeadLines         ="Grey's Hospital is a referral/tertiary hospital providing 100% tertiary services to patients and is located in Pietermaritzburg " ,
+                    HospBio         ="Grey's Hospital is a referral/tertiary hospital providing 100% tertiary services to patients and is located in Pietermaritzburg " ,
                      HeadLinesDesc     = "Microsoft Corporation (/ˈmaɪkrəˌsɒft/,[2][3] abbreviated.............small sample based on your scenario. Please review my project and let us know",
 
                       ProfileImage     = "Person_3.png"
@@ -60,7 +60,7 @@ namespace ProjectPanda.ViewModels
                        new MedicalBuildingDataModel
                     {
                     HeadTitle = " 13 hrs ago  - MacRumors ",
-                      HeadLines    = "MacRumors.com is a website that aggregates Mac and Apple",
+                      HospBio    = "MacRumors.com is a website that aggregates Mac and Apple",
                      HeadLinesDesc="Google Rolls Out Anti-Polishing Feature to Gmail on Ios",
                      ProfileImage = "Person_4.jpg"
 
@@ -71,7 +71,7 @@ namespace ProjectPanda.ViewModels
                     {
                     HeadTitle= " 15 hrs ago - Android Authority",
                    HeadLinesDesc  = "If you have read Jules Verne’s Around the World...........small sample based on your scenario. Please review my project and let us know" ,
-                     HeadLines="Set it and forget it : 5 Things you should always automate on your phone" ,
+                     HospBio="Set it and forget it : 5 Things you should always automate on your phone" ,
                      ProfileImage = "Person_5.jpg"
                         },
 
@@ -80,7 +80,7 @@ namespace ProjectPanda.ViewModels
                     {
                       HeadTitle = " 16 hrs ago - MacRumors",
                       HeadLinesDesc = "MacRumors.com is a website that aggregates Mac and Apple related news................small sample based on your scenario. Please review my project and let us know,",
-                      HeadLines="Set it and forget it : 5 Things you should always automate on your phone" ,
+                      HospBio="Set it and forget it : 5 Things you should always automate on your phone" ,
                       ProfileImage = "Person_1.png"
                          },
 
@@ -89,7 +89,7 @@ namespace ProjectPanda.ViewModels
                     {
                      HeadTitle = " 19 hrs ago  - Android Authority",
                       HeadLinesDesc = "MacRumors.com is a website that aggregates Mac and Apple.........small sample based on your scenario. Please review my project and let us know",
-                      HeadLines="Set it and forget it : 5 Things you should always automate on your phone" ,
+                      HospBio="Set it and forget it : 5 Things you should always automate on your phone" ,
                       ProfileImage = "Person_2.png"
                           },
                    
