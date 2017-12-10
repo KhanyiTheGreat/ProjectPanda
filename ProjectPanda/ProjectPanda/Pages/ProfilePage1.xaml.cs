09cb028877d982d4ece9930fe680e5cc49329783
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-12-10 22:32:30 +0200

    new profile

--- ProjectPanda/ProjectPanda/Pages/ProfilePage1.xaml.cs
+++ ProjectPanda/ProjectPanda/Pages/ProfilePage1.xaml.cs
@@ -10,11 +10,50 @@ using Xamarin.Forms.Xaml;
 namespace ProjectPanda.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ProfilePage1 : ContentPage
+	public partial class ProfilePage1 : MasterDetailPage
 	{
 		public ProfilePage1 ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+
+        void Menu_btn2_Clicked(object sender, EventArgs e)
+        {
+
+            Detail = new NavigationPage(new Settings());
+
+            IsPresented = false;
+        }
+
+        void Menu_btn4_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new SupportPagae());
+            IsPresented = false;
+        }
+
+        void Menu_btn3_Clicked(object sender, EventArgs e)
+        {
+            // This button is for payment history
+            Detail = new NavigationPage(new PaymentHistory());
+            IsPresented = false;
+        }
+
+
+
+
+        void Button_Clicked(object sender, EventArgs e)
+           => new NavigationPage();//doctorlist
+
+
+
+        void Logout(object sender, EventArgs e)
+        {
+            //code that will allow for logging out.
+            new NavigationPage(new SignInPrompt());
+
+
+        }
+
+    }
 }
--- ProjectPanda/ProjectPanda/Pages/SignInPrompt.xaml.cs
+++ ProjectPanda/ProjectPanda/Pages/SignInPrompt.xaml.cs
@@ -35,7 +35,7 @@ namespace ProjectPanda.Pages
 
 
 
-           await Navigation.PushModalAsync(new MainProfile());
+           await Navigation.PushModalAsync(new ProfilePage1());
            // Navigation.PushAsync(new DetailPageForFilling());
      
         }
