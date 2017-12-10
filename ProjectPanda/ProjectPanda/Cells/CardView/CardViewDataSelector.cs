3a4f932179757dfb992859d0b05c000111fde3e0
Bonga <Bonga@DESKTOP-8R6GJO0>
2017-12-10 20:38:59 +0200

    other changes

---
+++ ProjectPanda/ProjectPanda/Cells/CardView/CardViewDataSelector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPanda.Cells.CardView
+{
+    public class CardViewDataSelector 
+    {
+        //readonly DataTemplate normalTemplate;
+    }
+}
