322b7b7fe21087f50a5d0fe4c6ca803778c26ac3
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-12-14 20:11:59 +0200

    added some patient code

--- ProjectPanda/ProjectPanda/Models/patientUser.cs
+++ ProjectPanda/ProjectPanda/Models/patientUser.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace ProjectPanda.Models
 {
-    class patientUser
+    class patientUser:GpsLocator
     {
+        //this class must caluculate the distance between the user and the doctor/practice
+        //1 it must get the gps location from the medicalbuildingdatamodel
+        //2 it must get the user current location
+        //3 it must then do the conversions from the location from gps format to int
 
 
     }
+
+
+
+    public class DistanceAlgorithm
+    {
+        //this method uses the haversine algorithim
+
+        #region distance calculator 
+        //method for calulating distance between user and location
+        public string DistanceCalculator()
+        {
+
+
+
+
+            string distance = "";
+
+
+
+            return distance;
+        }
+        #endregion
+
+    }
+
 }
