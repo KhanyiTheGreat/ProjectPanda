9b4dd94cc9f05cc179e3b6b7388fcb912045d89d
Bonga <Bonga@DESKTOP-8R6GJO0>
2017-12-20 23:21:36 +0200

    bottom-tab page

---
+++ ProjectPanda/ProjectPanda/Demo(Delete when done)/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectPanda.Demo_Delete_when_done_
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            //var page = new Page1();
+            //Placeholder.Content = page.Content;
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped_4(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
